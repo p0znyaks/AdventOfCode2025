3487d9108569baae6f2fb7762f9cab5e11081d9e
p0znyaks <p0zny4ks@yandex.ru>
2025-12-05 22:43:43 +0300

    Garbage code remove

--- src/ProblemSolver.java
+++ src/ProblemSolver.java
@@ -5,12 +5,5 @@ public class ProblemSolver {
         DialSolverPart1 dialSolverPart1 = new DialSolverPart1();
 
         System.out.println(dialSolverPart1.solveDial());
-
-        int dial = 50;
-        int distance = 1000;
-
-        for (int i = 0; i < distance; i++) {
-            if (dial)
-        }
     }
 }
