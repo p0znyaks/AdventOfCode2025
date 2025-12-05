59f481dc6a1eea140852b322b4b902e654262ca3
p0znyaks <p0zny4ks@yandex.ru>
2025-12-06 00:18:06 +0300

    First part of Day2 problem solved

---
+++ src/Day2GiftShop/InvalidIdsRecognizer.java
@@ -0,0 +1,40 @@
+package Day2GiftShop;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class InvalidIdsRecognizer {
+    long sum;
+
+    public long recognizeInvalidIds() {
+        try {
+            String[] ranges = Files.readString(Paths.get("src/Day2GiftShop/input.txt")).split(",");
+
+            for (String range : ranges) {
+                String[] parts = range.split("-");
+
+                long start = Long.parseLong(parts[0]);
+                long end = Long.parseLong(parts[1]);
+
+                for (; start <= end; start++) {
+                    String numberStr = String.valueOf(start);
+
+                    if (numberStr.length() % 2 != 0) {
+                    } else {
+                        int half = numberStr.length() / 2;
+                        String firstHalf = numberStr.substring(0, half);
+                        String secondHalf = numberStr.substring(half);
+                        if ((firstHalf.equals(secondHalf)) && numberStr.charAt(0) != '0') {
+                            sum += start;
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file", e);
+        }
+
+        return sum;
+    }
+}
